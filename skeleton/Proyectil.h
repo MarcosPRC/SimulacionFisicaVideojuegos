ca27d26e2aae928640c4d0e6354fb83c8ed83f76
[MarcosPRC] <marcopan@ucm.es>
2024-10-24 22:13:55 +0200

    proyectil deberia funcionar

---
+++ skeleton/Proyectil.h
@@ -0,0 +1,46 @@
+#pragma once
+#include "Particle.h"
+
+const double gravedad = 9.8;
+
+class Proyectil
+{
+private:
+    Particle* particula;
+    double masaReal;
+    double masaEscalada;
+    double velocidadEscalada;
+    double velocidadReal;
+    double gravedadEscalada;
+
+public:
+    Proyectil(double velocidadInicial, double masaInicial, Vector3 posicion, Vector3 inicio): masaReal(masaInicial), velocidadReal(velocidadInicial) {
+
+        velocidadEscalada = 10.0;  
+        calcularMasaEscalada();   // Calcula la masa escalada.
+        ajustarGravedad();        // Ajusta la gravedad en función de la velocidad
+
+        Vector3 direccion = posicion;
+        direccion.normalize();
+
+        PxTransform* pose = new PxTransform(inicio);  // PxTransform para la posición.
+        particula = new Particle(pose, direccion * velocidadEscalada, Vector3(0, -gravedadEscalada, 0));
+    };
+
+    void calcularMasaEscalada() {
+        masaEscalada = masaReal * (pow(velocidadReal, 2) / pow(velocidadEscalada, 2));
+    }
+
+    // ajusta la gravedad escalada segun las velocidades
+    void ajustarGravedad() {
+        gravedadEscalada = pow(velocidadEscalada, 2) * gravedad / pow(velocidadReal, 2);
+    }
+
+    void disparar(double tiempo) {
+        particula->integrate(tiempo);
+    }
+
+
+};
+
+
